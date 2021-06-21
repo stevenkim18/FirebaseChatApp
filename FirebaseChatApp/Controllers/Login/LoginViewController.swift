fcd23b9375813b83f7b9634a6a8cbf5b9c2c7527
stevenkim18 <11882@naver.com>
2021-06-21 19:20:57 +0900

    feat: 로고 이미지 띄우기

--- FirebaseChatApp/Controllers/Login/LoginViewController.swift
+++ FirebaseChatApp/Controllers/Login/LoginViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        // TODO: 이미지 콘텐츠 모드 공부
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Log In"
@@ -18,6 +26,17 @@ class LoginViewController: UIViewController {
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
+        view.addSubview(logoImageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.frame.size.width/3
+        logoImageView.frame = CGRect(x: (view.frame.size.width-size)/2,
+                                     y: 20,
+                                     width: size,
+                                     height: size)
+        
     }
     
     @objc private func didTapRegister() {
