6f559fd46a7b0c824f8bd0c7ca604972ea2a786d
stevenkim18 <11882@naver.com>
2021-06-22 14:17:45 +0900

    feat: 이메일 입력창 구현

--- FirebaseChatApp/Controllers/Login/LoginViewController.swift
+++ FirebaseChatApp/Controllers/Login/LoginViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
     private let logoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "logo")
         // TODO: 이미지 콘텐츠 모드 공부
         imageView.contentMode = .scaleAspectFit
         return imageView
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email Address.."
+        
+        // 입력창에 왼쪽으로 딱 붙어 있는것을 조금 띄워주는 방법
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
     }()
     
     override func viewDidLoad() {
@@ -26,16 +49,25 @@ class LoginViewController: UIViewController {
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
-        view.addSubview(logoImageView)
+        // Add subviews
+        view.addSubview(scrollView)
+        scrollView.addSubview(logoImageView)
+        scrollView.addSubview(emailField)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.frame.size.width/3
-        logoImageView.frame = CGRect(x: (view.frame.size.width-size)/2,
+        scrollView.frame = view.bounds
+        
+        let size = scrollView.width/3
+        logoImageView.frame = CGRect(x: (scrollView.width-size)/2,
                                      y: 20,
                                      width: size,
                                      height: size)
+        emailField.frame = CGRect(x: 30,
+                                  y: logoImageView.bottom + 20,
+                                  width: scrollView.width - 60,
+                                  height: 52)
         
     }
     
